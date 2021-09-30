8d93ddd834a6bfb43fdf46631fb746d6f79be3d0
Matas <nilenon@gmail.com>
2021-09-30 20:16:14 +0700

    init project

--- controllers/search/getItems.js
+++ controllers/search/getItems.js
@@ -2,7 +2,9 @@ const { GoogleSpreadsheet } = require('google-spreadsheet');
 const { client_email, private_key } = require('../../config/cred')
 const moment = require('moment-timezone')
 
-
+const monthNames = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+];
 exports.getItems = async (req, res) => {
   const twitter = req.params.twitter
   const tel = req.params.tel
@@ -15,15 +17,22 @@ exports.getItems = async (req, res) => {
   const sheet = doc.sheetsByTitle["รวมทั้งหมด"];
   const rows = await sheet.getRows();
   const data = await getDataPromise(rows, twitter)
-  const sortedData = data.sort((a, b) => (a.release_date_compare > b.release_date_compare) ? 1 : -1)
+  const sortedData = data.sort((a, b) => (b.release_date_compare > a.release_date_compare) ? 1 : -1)
   const groupedData = await groupDate(sortedData)
-  res.json({ success: true, data: groupedData });
+  const resData = []
+  Object.keys(groupedData).map((key) => {
+    // let item = {key : null , value : null}
+    // item[key] = [...groupedData[key]];
+    resData.push({ key: key, value: [...groupedData[key]] })
+  });
+  res.json({ success: true, data: resData });
 }
 
 const getDataPromise = async (rows, account) => {
   const data = [];
   for (let i = 0; i < rows.length; i++) {
     if (rows[i]["@Twitter"] === account) {
+
       data.push({
         "id": i,
         "shipping_method": rows[i]["ขนส่ง"] || null,
@@ -37,6 +46,7 @@ const getDataPromise = async (rows, account) => {
         "amount": rows[i]['รวมจำนวนกี่ชิ้น/เซ็ต'] || null,
         "product_name": rows[i]['รายการสั่งซื้อ'] || null,
         "Timestamp": rows[i]['Timestamp'] || null,
+        "release_date_format": `${monthNames[new Date(rows[i]["เว็บจัดส่งภายในวันที่"]).getMonth()]} ${new Date(rows[i]["เว็บจัดส่งภายในวันที่"]).getFullYear()}`,
         "release_date_compare": Date.parse(rows[i]["เว็บจัดส่งภายในวันที่"]),
         // "date": date,
         "success": true,
@@ -52,7 +62,7 @@ const groupDate = (rows) => {
   let group = rows.reduce((r, a) => {
     console.log("a", a);
     console.log('r', r);
-    r[a.release_date] = [...r[a.release_date] || [], a];
+    r[a.release_date_format] = [...r[a.release_date_format] || [], a];
     return r;
   }, {});
   return group;

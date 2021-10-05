862386d354b95307084f15603da25fa3f1b354e1
Matas <nilenon@gmail.com>
2021-10-05 19:53:10 +0700

    add patch manage

--- controllers/manage/patchOrderById.js
+++ controllers/manage/patchOrderById.js
@@ -5,45 +5,38 @@ const { client_email, private_key } = require('../../config/cred')
 const moment = require('moment-timezone')
 exports.patchOrderById = async (req, res, next) => {
     console.log("patch", req.params.id)
-    console.log(req.body)
-    //     try {
-    //         const date = req.query.date
-    //         const shop = "catchy_kr"
-    //         const data = req.body
-    //         const id = req.params.id
-    //         console.log(moment().tz("Asia/Bangkok").toString(), ` - patchOrderById ${date},${shop} ,${id}`)
-    //         const sheetId = getSheetsId(shop)
-    //         const doc = new GoogleSpreadsheet(sheetId);
-    //         await doc.useServiceAccountAuth({
-    //             client_email: client_email,
-    //             private_key: private_key.replace(new RegExp("\\\\n", "\g"), "\n"),
-    //         });
-    //         await doc.loadInfo();
-    //         const sheet = doc.sheetsByTitle[date];
-    //         const rows = await sheet.getRows();
-    //         rows[id]['Tracking no.'] = data.tracking_no,
-    //             rows[id]['รายการ'] = data.product_name,
-    //             rows[id]['จำนวน'] = data.amount,
-    //             rows[id]['การจัดส่ง'] = data.shipping_method,
-    //             rows[id]['ที่อยู่'] = data.address,
-    //             rows[id]['ยอดที่โอน'] = data.pay_amount
-    //         rows[id]['สถานะสินค้า'] = data.product_status
-    //         rows[id]['สถานะการจ่ายเงิน'] = data.payment_status
-    //         rows[id]['Note'] = data.note
-    //         rows[id]['ต้นทุน'] = data.cost
-    //         rows[id]['ราคาขาย'] = data.price
-    //         rows[id]['ค่าส่งที่เก็บ'] = data.shipping_price,
-    //             rows[id]['ค่าส่งจริง'] = data.shipping_price_real
-    //         rows[id]['ชื่อ'] = data.name
-    //         rows[id]['เบอร์โทรศัพท์'] = data.telno
-    //         rows[id]['ยอดมัดจำที่เหลือ'] = data.total2
-    //         await rows[id].save();
-    //         await patchOrderById(date, shop, data, id)
-    //         res.status(200).json({ success: true, method: 'patch orders ' + req.params.id });
-    //     } catch (err) {
-    //         res.status(500).json({ success: false, code: err.response ? err.response.status : 405 });
-    //     }
-    // }
+    const id = req.params.id;
+    const data = req.body;
+    try {
+        const doc = new GoogleSpreadsheet(process.env.NANA_SHEET);
+        await doc.useServiceAccountAuth({
+            client_email: client_email,
+            private_key: private_key.replace(new RegExp("\\\\n", "\g"), "\n"),
+        });
+        await doc.loadInfo();
+        const sheet = doc.sheetsByTitle["รวมทั้งหมด"];
+        const rows = await sheet.getRows();
+        rows[id]['@Twitter'] = data.twitter,
+            rows[id]['รายการสั่งซื้อ'] = data.product_name,
+            rows[id]['รวมจำนวนกี่ชิ้น/เซ็ต'] = data.amount,
+            rows[id]['รวมราคา'] = data.total_amount,
+            rows[id]['เต็มจำนวน/มัดจำ'] = data.payment_method,
+            rows[id]['จ่ายแล้ว'] = data.already_paid
+        rows[id]['คงเหลือ'] = data.total2
+        rows[id]['จ่ายที่เหลือภายในวันที่'] = data.paid_date || ''
+        rows[id]['เว็บจัดส่งภายในวันที่'] = data.release_date
+        rows[id]['ขนส่ง'] = data.shipping_method
+        rows[id]['กดสั่งซื้อ'] = data.product_status
+        rows[id]['ชื่อ-ที่อยู่-เบอร์โทร'] = data.address,
+            rows[id]['หมายเหตุ'] = data.caution || ''
+        await rows[id].save();
+        // await patchOrderById(date, shop, data, id)
+        res.status(200).json({ success: true, method: 'patch orders ' + req.params.id });
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({ success: false, code: err.response ? err.response.status : 405 });
+    }
+}
 
 
     // const patchOrderById = async (date, shop, data, id) => {
@@ -67,5 +60,5 @@ exports.patchOrderById = async (req, res, next) => {
     //     } catch (err) {
     //         console.log(err)
     //     }
-    res.status(200).json({ success: true, method: 'patch orders ' + req.params.id });
-}
+//     res.status(200).json({ success: true, method: 'patch orders ' + req.params.id });
+// }
